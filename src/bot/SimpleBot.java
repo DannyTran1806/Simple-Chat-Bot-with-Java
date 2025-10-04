c02274167f4c4fec840e30e671affa8282ebaaf6
DannyTran1806 <dannytran1806@gmail.com>
2025-10-04 19:52:23 +0200

    feature: bot guesses age

--- src/bot/SimpleBot.java
+++ src/bot/SimpleBot.java
@@ -3,15 +3,26 @@ import java.util.Scanner;
 
 public class SimpleBot {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        String botName = "JARVIS";
-        String birthYear = "2025";
-        System.out.println("Hello! My name is " + botName + ".");
-        System.out.println("I was created in " + birthYear + ".");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello! My name is Aid.");
+        System.out.println("I was created in 2018.");
         System.out.println("Please, remind me your name.");
-        System.out.print("> ");
-        String name = input.nextLine();
+
+        String name = scanner.nextLine();
+
         System.out.println("What a great name you have, " + name + "!");
-        input.close();
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+
+        // reading all remainders
+        int remainder3 = scanner.nextInt();
+        int remainder5 = scanner.nextInt();
+        int remainder7 = scanner.nextInt();
+
+        int yourAge = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+
+        System.out.println("Your age is " + yourAge + "; that's a good time to start programming!");
+        scanner.close();
     }
 }
