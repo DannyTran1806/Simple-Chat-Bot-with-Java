f2174de0b5cbcc087f3048e1c40557809ea8c702
DannyTran1806 <dannytran1806@gmail.com>
2025-10-04 14:08:22 +0200

    reading user's input with Scanner

--- src/bot/SimpleBot.java
+++ src/bot/SimpleBot.java
@@ -1,10 +1,17 @@
 package bot;
+import java.util.Scanner;
 
 public class SimpleBot {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         String botName = "JARVIS";
         String birthYear = "2025";
         System.out.println("Hello! My name is " + botName + ".");
         System.out.println("I was created in " + birthYear + ".");
+        System.out.println("Please, remind me your name.");
+        System.out.print("> ");
+        String name = input.nextLine();
+        System.out.println("What a great name you have, " + name + "!");
+        input.close();
     }
 }
