7ef7a15d700c55bd6ad6ca8ce9bd503fd1809c41
DannyTran1806 <dannytran1806@gmail.com>
2025-10-04 14:00:01 +0200

    Simple Chat Bot print statements

---
+++ src/bot/SimpleBot.java
@@ -0,0 +1,10 @@
+package bot;
+
+public class SimpleBot {
+    public static void main(String[] args) {
+        String botName = "JARVIS";
+        String birthYear = "2025";
+        System.out.println("Hello! My name is " + botName + ".");
+        System.out.println("I was created in " + birthYear + ".");
+    }
+}
